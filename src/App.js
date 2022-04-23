bfa686bc003d783bdfe055d58d79750b398bda64
Chandraprakash-Darji <prakashchandra3786@gmail.com>
2022-04-23 18:14:01 +0530

    Class ComponentAdded

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Greet from "./Components/Greet";
+import Welcome from "./Components/Welcome";
 
 function App() {
   return (
     <div className="App">
       <Greet />
+      <Welcome/>
     </div>
   );
 }

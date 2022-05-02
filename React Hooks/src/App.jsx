c333b4503599a14337025bfdf4f368511450894d
Chandraprakash-Darji <prakashchandra3786@gmail.com>
2022-05-02 21:51:43 +0530

    useEffect

--- React Hooks/src/App.jsx
+++ React Hooks/src/App.jsx
@@ -1,7 +1,11 @@
+import FunCounter from "./components/useEffect/FunCounter";
+import ClassCounter from "./components/useEffect/ClassCounter";
+
 const App = () => {
   return (
     <div>
-                                                             
+      {/* <ClassCounter /> */}
+      <FunCounter />
     </div>
   );
 };
---
+++ React Hooks/src/components/useEffect/FunCounter.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+
+const FunCounter = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Clicked ${count} times`;
+  }, [count]);
+
+  return (
+    <div>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Click {count} times
+      </button>
+    </div>
+  );
+};
+
+export default FunCounter;

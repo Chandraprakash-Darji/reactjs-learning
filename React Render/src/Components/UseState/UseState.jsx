2f9dfe4de8a4a69e5c05d9805f8a5c76b752aa6b
Chandraprakash-Darji <prakashchandra3786@gmail.com>
2022-05-03 21:19:42 +0530

    React Render

---
+++ React Render/src/Components/UseState/UseState.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const UseState = () => {
+  const [count, setCount] = useState(0);
+  console.log("UseState Render");
+  return (
+    <div>
+      <button onClick={() => setCount((c) => c + 1)}>Count = {count}</button>
+      {/* Only Update the render Once Because set remains same
+      when there is no state change then it not flag as updateable
+      hence component remains same */}
+      <button onClick={() => setCount(0)}>Count = 0</button>
+      <button onClick={() => setCount(5)}>Count = 5</button>
+    </div>
+  );
+};
+
+export default UseState;

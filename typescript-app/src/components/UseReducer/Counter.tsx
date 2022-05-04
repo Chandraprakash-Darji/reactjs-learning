564d159f547de1f52c058c0eca86dadf096630ad
Chandraprakash-Darji <prakashchandra3786@gmail.com>
2022-05-04 18:56:39 +0530

    Strict ACtion type in UseReducer

--- typescript-app/src/components/UseReducer/Counter.tsx
+++ typescript-app/src/components/UseReducer/Counter.tsx
@@ -4,10 +4,16 @@ type CounterState = {
   count: number;
 };
 
-type CounterAction = {
-  type: string;
+type UpdateAction = {
+  type: "increment" | "decrement";
   value: number;
 };
+
+type ResetAction = {
+  type: "reset";
+};
+
+type CounterAction = UpdateAction | ResetAction;
 
 const initialState = { count: 0 };
 const reducer = (state: CounterState, action: CounterAction) => {
@@ -16,8 +22,8 @@ const reducer = (state: CounterState, action: CounterAction) => {
       return { count: state.count + action.value };
     case "decrement":
       return { count: state.count - action.value };
-    default:
-      return state;
+    case "reset":
+      return initialState;
   }
 };
 
@@ -28,11 +34,12 @@ const Counter = () => {
     <div>
       <h2>{count.count}</h2>
       <button onClick={() => dispatch({ type: "increment", value: 1 })}>
-        Increment 1
+        Increment
       </button>
       <button onClick={() => dispatch({ type: "decrement", value: 1 })}>
-        Decrement 1
+        Decrement
       </button>
+      <button onClick={() => dispatch({ type: "reset" })}>reset</button>
     </div>
   );
 };

5b2aafb6bfeb835632739af2e99897abde9fcb61
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-05-07 21:40:49 +0530

    Polymorhic Components

--- typescript-app/src/App.tsx
+++ typescript-app/src/App.tsx
@@ -1,7 +1,13 @@
+import Text from "./components/polymorphic/Text";
+
 function App() {
   return (
     <div className="App">
-
+      <Text as='h1' size="lg">Heading</Text>
+      <Text as='p' size="md">Paragraph</Text>
+      <Text as='label' htmlFor="email" size="sm" color="secondary">
+        Label
+      </Text>
     </div>
   );
 }

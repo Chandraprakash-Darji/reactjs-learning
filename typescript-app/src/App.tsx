3f2b3c2b35506a2bcc59d58e07581a04a94f77be
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-05-07 19:38:09 +0530

    Limit Generic Type

--- typescript-app/src/App.tsx
+++ typescript-app/src/App.tsx
@@ -4,11 +4,6 @@ function App() {
   return (
     <div className="App">
       <List
-        items={["Chandraprakash", "Moon Light", "Rega", "CP", "Chandu"]}
-        onClick={(item) => console.log(item)}
-      />
-      <List items={[1, 2, 3, 4, 5]} onClick={(item) => console.log(item)} />
-      <List
         items={[
           { first: "C", last: "P" },
           { first: "Chandra", last: "Prakash" },
--- typescript-app/src/components/generics/List.tsx
+++ typescript-app/src/components/generics/List.tsx
@@ -1,14 +1,17 @@
 type ListProps<T> = {
   items: T[];
-  onClick: (value:T) => void;
+  onClick: (value: T) => void;
 };
-const List = <T extends {}>({ items, onClick }: ListProps<T>) => {
+const List = <T extends { first: string; last: string }>({
+  items,
+  onClick,
+}: ListProps<T>) => {
   return (
     <div>
       <h2>List of Items</h2>
       {items.map((item, index) => (
         <div key={`${item}${index}`} onClick={() => onClick(item)}>
-          {index}
+          {item.first} {item.last}
         </div>
       ))}
     </div>

3bf740be17d112be6bf47a3b29d803d9c14434ab
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-05-08 18:37:00 +0530

    Auto Complete with Array of Objects

--- react-material-ui/src/components/MuiAutocomplete.tsx
+++ react-material-ui/src/components/MuiAutocomplete.tsx
@@ -1,15 +1,23 @@
 import { Autocomplete, Stack, TextField } from "@mui/material";
 import { useState } from "react";
 
-
+type Skill = {
+  id: number;
+  label: string;
+};
 
 const skills = ["Html", "CSS", "Javascript", "TypeScript", "React"];
 
-
+const skillsOption = skills.map((skill, index) => ({
+  id: index + 1,
+  label: skill,
+}));
 
 const MuiAutocomplete = () => {
   const [value, setValue] = useState<string | null>(null);
-  console.log({ value });
+
+  const [skill, setSkill] = useState<Skill | null>(null);
+  console.log({ skill });
   return (
     <Stack spacing={2} width="250px">
       <Autocomplete
@@ -18,6 +26,12 @@ const MuiAutocomplete = () => {
         value={value}
         onChange={(_event: any, newVal: string | null) => setValue(newVal)}
         freeSolo
+      />
+      <Autocomplete
+        options={skillsOption}
+        renderInput={(parmas) => <TextField {...parmas} label="Skills" />}
+        value={skill}
+        onChange={(_event: any, newVal: Skill | null) => setSkill(newVal)}
       />
     </Stack>
   );

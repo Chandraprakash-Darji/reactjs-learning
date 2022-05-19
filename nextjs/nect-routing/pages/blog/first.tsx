16674254cd9cff9fce02c067dbaaec1493dbdba9
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-05-19 18:38:12 +0530

    Nested Routes

---
+++ nextjs/nect-routing/pages/blog/first.tsx
@@ -0,0 +1,7 @@
+const FirstBlogPage = () => {
+  return (
+    <h2>FirstBlogPage</h2>
+  )
+}
+
+export default FirstBlogPage
---
+++ nextjs/nect-routing/pages/blog/second.tsx
@@ -0,0 +1,7 @@
+const SecondBlogPage = () => {
+  return (
+    <h2>Second Blog Page</h2>
+  )
+}
+
+export default SecondBlogPage

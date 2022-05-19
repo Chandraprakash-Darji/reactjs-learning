f51f48ea1bef86c383e90cf45400007a1f9b99f2
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-05-19 20:49:16 +0530

    Navigating Programatically

--- nextjs/nect-routing/pages/index.tsx
+++ nextjs/nect-routing/pages/index.tsx
@@ -1,15 +1,22 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Home = () => {
+    const router = useRouter();
+    const handleClick = () => {
+        console.log("Place Orde Success");
+        router.push("/product");
+    };
     return (
         <>
-            <h1>Home Page</h1>;
+            <h1>Home Page</h1>
             <Link href={"/blog"}>
                 <a>Blog</a>
             </Link>
             <Link href={"/product"}>
                 <a>Products</a>
             </Link>
+            <button onClick={handleClick}>Place Order</button>
         </>
     );
 };

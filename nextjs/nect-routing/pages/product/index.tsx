aedbb5ec488aac79fedc37af6b88e631b8a46ae0
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-05-19 19:36:31 +0530

    Link Component Navigation

--- nextjs/nect-routing/pages/product/index.tsx
+++ nextjs/nect-routing/pages/product/index.tsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
-const ProductList = () => {
-  return (
-    <>
-     <h2>Product 1</h2>   
-     <h2>Product 2</h2>   
-     <h2>Product 3</h2>   
-    </>
-  )
-}
+const ProductList = ({ productId = 100 }) => {
+    return (
+        <>
+            <Link href={"/"}>
+                <a>Home</a>
+            </Link>
+            <h2>
+                <Link href={"/product/1"}>
+                    <a>Product 1</a>
+                </Link>
+            </h2>
+            <h2>
+                <Link href={"/product/2"}>
+                    <a>Product 2</a>
+                </Link>
+            </h2>
+            <h2>
+                <Link href={"/product/3"} replace>
+                    <a>Product 3</a>
+                </Link>
+            </h2>
+            <h2>
+                <Link href={`/product/${productId}`}>
+                    <a>Product {productId}</a>
+                </Link>
+            </h2>
+        </>
+    );
+};
 
-export default ProductList
+export default ProductList;
